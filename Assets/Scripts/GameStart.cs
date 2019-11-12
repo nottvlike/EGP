62678c528127c18786d4b2e2635c5db8b6781078
nottvlike <nottvlike@gmail.com>
2019-11-12 22:57:24 +0800

    补充game start

--- Assets/Scripts/GameStart.cs
+++ Assets/Scripts/GameStart.cs
@@ -1,6 +1,13 @@
-﻿using UnityEngine;
+﻿using ECS.Common;
 using ECS;
 using ECS.Config;
+using ECS.UI;
+using ECS.Module;
+using Asset;
+using UnityEngine;
+using UniRx;
+using System.Threading.Tasks;
+using System;
 
 public abstract class GameStart : MonoBehaviour
 {
@@ -9,14 +16,29 @@ public abstract class GameStart : MonoBehaviour
         LoadGame();
     }
 
-    protected void LoadGame()
+    protected async void LoadGame()
     {
-        var moduleGroupTypeConfig = Resources.Load<LayerMaskConfig>("Config/ModuleGroupTypeConfig");
+        var moduleGroupTypeConfigPath = AssetPath.GetAssetPathFromResourcePath(AssetConstant.MODULE_GROUP_TYPE_CONFIG_PATH);
+        var moduleGroupTypeConfig = Resources.Load<LayerMaskConfig>(moduleGroupTypeConfigPath);
+        
         var worldMgr = WorldManager.Instance;
         worldMgr.Module.Init(moduleGroupTypeConfig);
+        
+        var unitTypeConfigPath = AssetPath.GetAssetPathFromResourcePath(AssetConstant.UNIT_TYPE_CONFIG_PATH);
+        var unitTypeConfig = Resources.Load<LayerConfig>(unitTypeConfigPath);
 
-        var unitTypeConfig = Resources.Load<LayerConfig>("Config/UnitTypeConfig");
         worldMgr.Unit.Init(unitTypeConfig);
+
+#if UNITY_EDITOR
+        AssetManager.IsSimulate = true;
+#endif
+        AssetManager.CDN = "http://localhost:8000/AssetBundles";
+
+        var assetPath = AssetPath.GetAssetPathFromResourcePath(AssetConstant.ASSET_CONFIG_PATH);
+        var assetCofig = Resources.Load<AssetConfig>(assetPath);
+        await AssetManager.Init(assetCofig);
+
+        UIManager.Init();
 
         RegisterGameModule();
         
