621bc69cdd81c0df2dbcd43f1fd5555131b589db
nottvlike <nottvlike@gmail.com>
2019-11-16 16:54:36 +0800

    优化 PanelMode

--- ECS/UI/Script/UIConstant.cs
+++ ECS/UI/Script/UIConstant.cs
@@ -3,9 +3,8 @@
     public enum PanelMode
     {
         None = 0,
-        Alone,          //独占的独立面板
-        Child,          //独立子面板
-        Popover         //确认框等面板
+        Alone,
+        Popup
     }
 
     internal class UIConstant
