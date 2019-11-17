a4da8b6a36a6305120a00449e96ccb67aeee6264
nottvlike <nottvlike@gmail.com>
2019-11-17 11:41:06 +0800

    修复报错

--- Assets/Editor/BuildHelper.cs
+++ Assets/Editor/BuildHelper.cs
@@ -1,50 +1,11 @@
 namespace Game.Editor
 {
     using Asset;
-    using Asset.Builder;
-    using AssetEditor;
     using UnityEditor;
     using UnityEngine;
-    using System.Collections.Generic;
 
-    [InitializeOnLoad]
     public class BuildHelper
     {
-        static BuildHelper()
-        {
-            // AssetBundleBuilder.AddCondition(
-            //     new ConditionInfo()
-            //     {
-            //         conditionList = new List<ICondition>()
-            //         {
-            //             new FileSizeCondition(0.5f),
-            //             new FileExtensionCondition(new List<string>()
-            //             {
-            //                 ".png",
-            //                 ".jpg",
-            //                 ".ttf",
-            //                 ".wav",
-            //                 ".ogg",
-            //                 ".mp3",
-            //                 ".xmp",
-            //                 ".fbx",
-            //             }),
-            //         }
-            //     });
-
-            AssetBundleBuilder.AddCondition(
-                new ConditionInfo()
-                {
-                    conditionList = new List<ICondition>()
-                    {
-                        new DirectoryCondition(new List<string>()
-                        {
-                            "Prefabs/Actor",
-                        }),
-                    }
-                });
-        }
-
         [MenuItem("Tools/Build Standalone Asset Bundle")]
         public static void BuildStandaloneAssetBundle()
         {
