7f1726afc40d4d0944ea83d497e5d3a07b6731ae
nottvlike <nottvlike@gmail.com>
2019-12-21 20:23:39 +0800

    添加UIAnimationProcessor
    添加UIAnimationProcessor;
    其中添加OnAnimationPlay和OnAnimationCommpleted主要是因为帧动画挂载的事件只能是Animator组件所在节点上的所有组件的部分方法，所以需要从这里中转到UIAnimationTrigger里。

---
+++ ECS/UI/Script/Animation/UIAnimationProcessor.cs
@@ -0,0 +1,54 @@
+namespace Animation 
+{
+    using UnityEngine;
+    using UnityEngine.Events;
+    using System.Collections.Generic;
+    using UniRx;
+    using System.Linq;
+    using ECS.Common;
+
+    public sealed class UIAnimationProcessor : MonoBehaviour
+    {
+        public List<UIAnimationTrigger> animationTriggerList;
+
+        void Reset() 
+        {
+            animationTriggerList.Clear();
+
+            var triggerList = GetComponentsInChildren<UIAnimationTrigger>(true);
+            animationTriggerList.AddRange(triggerList.Where(_ => !string.IsNullOrEmpty(_.GroupName)));
+        }
+
+        public void Play(string groupName, UnityAction onComplete = null)
+        {
+            var animationTrigger = animationTriggerList.Where(_ => _.GroupName == groupName).FirstOrDefault();
+#if DEBUG
+            if (animationTrigger == null)
+            {
+                Log.E("Failed to find animation of group {0}", groupName);
+                return;
+            }
+#endif
+
+            if (onComplete != null)
+            {
+                animationTrigger.onComplete.RemoveAllListeners();
+                animationTrigger.onComplete.AddListener(onComplete);
+            }
+
+            animationTrigger.Play();
+        }
+
+        public void OnAnimationPlay(string groupName)
+        {
+            var animationTrigger = animationTriggerList.Where(_ => _.GroupName == groupName).FirstOrDefault();
+            animationTrigger.OnAnimationPlay();
+        }
+
+        public void OnAnimationCommpleted(string groupName)
+        {
+            var animationTrigger = animationTriggerList.Where(_ => _.GroupName == groupName).FirstOrDefault();
+            animationTrigger.OnAnimationCommpleted();
+        }
+    }
+}
