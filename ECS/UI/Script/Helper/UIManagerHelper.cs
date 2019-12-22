b03e40c8eb8d6e318c5809a3bcc9233e422605b4
nottvlike <nottvlike@gmail.com>
2019-12-22 12:34:27 +0800

    添加UIManagerHelper

---
+++ ECS/UI/Script/Helper/UIManagerHelper.cs
@@ -0,0 +1,29 @@
+namespace Helper
+{
+    using UnityEngine;
+    using UnityEditor;
+    using ECS.UI;
+    using UniRx;
+
+    public sealed class UIManagerHelper : MonoBehaviour
+    {
+        public void Show(string assetPath)
+        {
+            UIManager.Show(assetPath).Subscribe();
+        }
+
+        public void Hide(string assetPath)
+        {
+            UIManager.Hide(assetPath);
+        }
+
+        public void Exit()
+        {
+#if UNITY_EDITOR
+            EditorApplication.isPlaying = false;
+#else
+		    Application.Quit();
+#endif    
+        }
+    }
+}
