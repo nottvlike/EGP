e48db8e873febce44aba60edf3604fb1b48a9684
nottvlike <nottvlike@gmail.com>
2019-12-31 23:17:13 +0800

    补充Destroy逻辑

--- ECS/Core/Script/Data/Unit/UnitData.cs
+++ ECS/Core/Script/Data/Unit/UnitData.cs
@@ -7,6 +7,7 @@
     {
         None = 0,
         Init,
+        Destroy,
     }
     
     public class UnitData : IData, IPoolObject
--- ECS/UI/Script/Factory/UIFactoryExtension.cs
+++ ECS/UI/Script/Factory/UIFactoryExtension.cs
@@ -4,28 +4,44 @@ namespace ECS.Factory
     using Data;
     using UnityEngine;
     using ECS.UI;
+    using GUnit = ECS.Unit.Unit;
     using Asset;
-    using ECS.Common;
 
     public static class UIFactoryExtension
     {
         public static void CreateUI(this UnitFactory factory, string assetPath,
-             GameObject asset, UIData uiData)
+             GameObject asset, UIData uiData = null)
         {
             var moduleMgr = WorldManager.Instance.Module;
             var requiredModuleGroup = moduleMgr.TagToModuleGroupType(Constant.UI_MODULE_GROUP_NAME) 
                 | moduleMgr.TagToModuleGroupType(Constant.UNIT_MODULE_GROUP_NAME);
             var unit = factory.CreateUnit(requiredModuleGroup);
 
+            var unitMgr = WorldManager.Instance.Unit;
+            if (uiData == null)
+            {
+                var uiCore = unitMgr.GetUnit(UIConstant.UI_CORE_UNIT_NAME);
+                uiData = uiCore.GetData<UIData>();
+            }
+
             var obj = asset.Spawn();
             obj.transform.SetParent(uiData.uiRoot.transform, false);
 
             var unitData = unit.GetData<UnitData>();
-            unitData.unitType = WorldManager.Instance.Unit.TagToUnitType(Constant.UI_UNIT_TYPE_NAME);
+            unitData.unitType = unitMgr.TagToUnitType(Constant.UI_UNIT_TYPE_NAME);
+            unitData.stateTypeProperty.Subscribe(_ => 
+            {
+                if (_ == UnitStateType.Destroy)
+                {
+                    factory.DestroyUIUnit(unit, uiData);
+                }
+            });
 
             var panel = obj.GetComponent<Panel>();
             unit.AddData(panel, typeof(Panel));
+
             var panelData = unit.AddData<PanelData>();
+            panelData.assetPath = assetPath;
             if (panelData.stateTypeProperty == null)
             {
                 panelData.stateTypeProperty = new ReactiveProperty<PanelStateType>(PanelStateType.None);
@@ -36,6 +52,23 @@ namespace ECS.Factory
             uiData.unitDict.Add(assetPath, unit);
 
             unitData.stateTypeProperty.Value = UnitStateType.Init;
+        }
+
+        public static void DestroyUIUnit(this UnitFactory factory, GUnit unit, UIData uiData = null)
+        {
+            if (uiData == null)
+            {
+                var uiCore = WorldManager.Instance.Unit.GetUnit(UIConstant.UI_CORE_UNIT_NAME);
+                uiData = uiCore.GetData<UIData>();
+            }
+
+            var panelData = unit.GetData<PanelData>();   
+            uiData.unitDict.Remove(panelData.assetPath);
+
+            var panel = unit.GetData<Panel>();
+            panel.gameObject.Despawn();
+
+            factory.DestroyUnit(unit);
         }
     }
 }
