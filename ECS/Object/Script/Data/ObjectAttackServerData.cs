28fb45aea44d1133cceea7d8fd8f35873e734dc4
nottvlike <nottvlike@gmail.com>
2020-02-29 19:15:44 +0800

    添加伤害计算模块

---
+++ ECS/Object/Script/Data/ObjectAttackServerData.cs
@@ -0,0 +1,53 @@
+﻿namespace ECS.Object.Data
+{
+    using ECS.Data;
+    using ECS.Common;
+    using UniRx;
+    using System;
+
+    public class AttackInfo : IPoolObject
+    {
+        public uint sourceId;
+        public uint targetId;
+        public int attack;
+
+        public bool IsInUse { get; set; }
+        public void Clear()
+        {
+            sourceId = 0;
+            targetId = 0;
+            attack = 0;
+        }
+    }
+
+    public class DamageInfo : IPoolObject
+    {
+        public uint sourceId;
+        public uint targetId;
+
+        public int attack;
+        public int damage;
+
+        public bool IsInUse { get; set; }
+        public void Clear()
+        {
+            sourceId = 0;
+            targetId = 0;
+            attack = 0;
+            damage = 0;
+        }
+    }
+
+    public class ObjectAttackServerData : IData, IPoolObject
+    {
+        public ISubject<AttackInfo> onAttack;
+        public ISubject<DamageInfo> onBeforeDamage;
+        public ISubject<DamageInfo> onDamage;
+        public ISubject<DamageInfo> onAfterDamage;
+
+        public bool IsInUse { get; set; }
+        public void Clear()
+        {
+        }
+    }
+}
---
+++ ECS/Object/Script/Module/ObjectAttackServer.cs
@@ -0,0 +1,96 @@
+﻿namespace ECS.Object.Module
+{
+    using ECS.Module;
+    using GUnit = ECS.Unit.Unit;
+    using ECS.Object.Data;
+    using ECS.Common;
+    using System;
+    using UniRx;
+
+    public class ObjectAttackServer : Module
+    {
+        public override int Group { get; protected set; }
+            = WorldManager.Instance.Module.TagToModuleGroupType(Constant.SYSTEM_MODULE_GROUP_NAME);
+
+        public ObjectAttackServer()
+        {
+            RequiredDataList = new Type[]{
+                typeof(ObjectAttackServerData)
+            };
+        }
+
+        static ObjectAttackServerData _attackServerData;
+
+        protected override void OnAdd(GUnit unit)
+        {
+            _attackServerData = unit.GetData<ObjectAttackServerData>();
+            _attackServerData.onAttack = new Subject<AttackInfo>();
+            _attackServerData.onBeforeDamage = new Subject<DamageInfo>();
+            _attackServerData.onDamage = new Subject<DamageInfo>();
+            _attackServerData.onAfterDamage = new Subject<DamageInfo>();
+        }
+
+        protected override void OnRemove(GUnit unit)
+        {
+            _attackServerData.onAttack.OnCompleted();
+            _attackServerData.onBeforeDamage.OnCompleted();
+            _attackServerData.onDamage.OnCompleted();
+            _attackServerData.onAfterDamage.OnCompleted();
+            _attackServerData = null;
+        }
+
+        public static IObservable<AttackInfo> ObserverAttack()
+        {
+            return Observable.Defer(() =>
+            {
+                return _attackServerData.onAttack;
+            });
+        }
+
+        public static IObservable<DamageInfo> ObserverBeforeDamage()
+        {
+            return Observable.Defer(() =>
+            {
+                return _attackServerData.onBeforeDamage;
+            });
+        }
+
+        public static IObservable<DamageInfo> ObserverDamage()
+        {
+            return Observable.Defer(() =>
+            {
+                return _attackServerData.onDamage;
+            });
+        }
+
+        public static IObservable<DamageInfo> ObserverAfterDamage()
+        {
+            return Observable.Defer(() =>
+            {
+                return _attackServerData.onAfterDamage;
+            });
+        }
+
+        public static DamageInfo Attack(GUnit source, AttackInfo attackInfo)
+        {
+            _attackServerData.onAttack.OnNext(attackInfo);
+
+            var damageInfo = Pool.Get<DamageInfo>();
+            damageInfo.sourceId = source.UnitId;
+            damageInfo.targetId = attackInfo.targetId;
+            damageInfo.attack = attackInfo.attack;
+            damageInfo.damage = attackInfo.attack;
+
+            return damageInfo;
+        }
+
+        public static void Damage(DamageInfo damageInfo)
+        {
+            _attackServerData.onBeforeDamage.OnNext(damageInfo);
+            _attackServerData.onDamage.OnNext(damageInfo);
+            _attackServerData.onAfterDamage.OnNext(damageInfo);
+
+            Pool.Release(damageInfo);
+        }
+    }
+}
