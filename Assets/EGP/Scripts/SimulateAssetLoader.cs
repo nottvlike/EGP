5933a0bd04b807b82b26514cce58ab96a4ad5f55
nottvlike <nottvlike@gmail.com>
2020-03-04 18:09:08 +0800

    添加 SimulateAssetLoader

---
+++ Assets/EGP/Scripts/SimulateAssetLoader.cs
@@ -0,0 +1,64 @@
+#if UNITY_EDITOR
+
+namespace AssetEditor
+{
+    using UnityEditor;
+    using UnityEngine;
+    using UObject = UnityEngine.Object;
+    using System;
+    using UniRx;
+    using Asset;
+    using ECS.Common;
+    
+    public class SimulateAssetLoader : IAssetLoader
+    {
+        AssetConfig _assetConfig;
+
+        public AssetBundleManifest Manifest => null;
+
+        public IObservable<Unit> Init()
+        {
+            return LoadAssetConfig().Do(assetConfig =>
+            {
+                _assetConfig = assetConfig;
+            }).AsUnitObservable();
+        }
+
+        IObservable<AssetConfig> LoadAssetConfig()
+        {
+            var assetFullPath = AssetDatabase.GetAssetPathsFromAssetBundleAndAssetName(
+                AssetConstant.ASSET_CONFIG_BUNDLE_NAME, AssetConstant.ASSET_CONFIG_ASSET_NAME);
+            return Observable.Return(AssetDatabase.LoadAssetAtPath<AssetConfig>(assetFullPath[0]),
+                    Scheduler.MainThreadIgnoreTimeScale);
+        }
+
+        public IObservable<T> Load<T>(string assetPath) where T : UObject
+        {
+            if (!_assetConfig.Contains(assetPath))
+            {
+                Log.I("Asset {0} not found in assetconfig ,refresh asset config first!", assetPath);
+                return Observable.Return<T>(null);
+            }
+
+            var assetInfo = _assetConfig.GetAssetInfo(assetPath);
+            if (!assetInfo.isFromBundle)
+            {
+                return Observable.Return(AssetDatabase.LoadAssetAtPath<T>(
+                    assetInfo.fullPath), Scheduler.MainThreadIgnoreTimeScale);
+            }
+            else
+            {
+                var assetFullPath = AssetDatabase.GetAssetPathsFromAssetBundleAndAssetName(
+                    assetInfo.bundleName, assetInfo.assetName);
+                return Observable.Return(AssetDatabase.LoadAssetAtPath<T>(assetFullPath[0]),
+                    Scheduler.MainThreadIgnoreTimeScale);
+            }
+        }
+
+        public void Clear(string assetPath)
+        {
+        }
+    }
+}
+
+#endif
