11532b4d37d08849e03ad095eb6dadf5dd74ce52
nottvlike <nottvlike@gmail.com>
2020-03-06 20:10:55 +0800

    Despawner移动到 Object

---
+++ ECS/Object/Script/Helper/Despawner.cs
@@ -0,0 +1,47 @@
+﻿namespace ECS.Object.Helper
+{
+    using UnityEngine;
+    using Asset;
+    using System;
+    using UniRx;
+
+    public class Despawner : MonoBehaviour
+    {
+        public float delay;
+        IDisposable _disposable;
+
+        void OnEnable()
+        {
+            Despawn();
+        }
+
+        void Dispose()
+        {
+            _disposable?.Dispose();
+            _disposable = null;
+        }
+
+        void Despawn()
+        {
+            if (delay <= 0)
+            {
+                return;
+            }
+
+            if (_disposable != null)
+            {
+                Dispose();
+            }
+
+            _disposable = Observable.Timer(TimeSpan.FromSeconds(delay)).Subscribe(_ =>
+            {
+                Dispose();
+
+                if (gameObject.activeInHierarchy)
+                {
+                    gameObject.Despawn();
+                }
+            });
+        }
+    }
+}
